87c9dd18bcc7132e2dbe60907b6721b78308b1b9
석성희 <sunghee119z@naver.com>
2022-05-19 01:37:34 +0900

    asdasd
    asdasd

--- src/main/java/dtalk/DtalkApplication.java
+++ src/main/java/dtalk/DtalkApplication.java
@@ -1,13 +1,13 @@
-package com.dtalk.api;
+package dtalk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiApplication {
+public class DtalkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
+		SpringApplication.run(DtalkApplication.class, args);
 	}
 
 }

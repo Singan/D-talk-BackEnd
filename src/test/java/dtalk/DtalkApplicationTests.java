eb74c1f7e7315329d757c3968ef523faf2de34df
석성희 <sunghee119z@naver.com>
2022-05-19 01:32:11 +0900

    .

---
+++ src/test/java/dtalk/DtalkApplicationTests.java
@@ -0,0 +1,13 @@
+package dtalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

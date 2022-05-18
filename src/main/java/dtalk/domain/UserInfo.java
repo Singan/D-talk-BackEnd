6e5cfb5f1fffba56d371fcc1c19be91161661d9d
석성희 <sunghee119z@naver.com>
2022-05-19 01:51:52 +0900

    asdas
    dasdasd

---
+++ src/main/java/dtalk/domain/UserInfo.java
@@ -0,0 +1,14 @@
+package dtalk.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class UserInfo {
+    @Id
+    @GeneratedValue
+    @Column(name = "user_info_idx")
+    private Long idx;
+}

dd3584c618eb41ec75d731cfce43d100ed1a8b83
Mi Reu Lee <im490113@gmail.com>
2022-11-27 22:54:13 +0900

    quiz detail get mapping url modify

--- src/main/java/dtalk/controller/QuizController.java
+++ src/main/java/dtalk/controller/QuizController.java
@@ -32,7 +32,7 @@ public class QuizController {
         return quizService.save(quizSaveDto);
     }
 
-    @GetMapping("/detail")
+    @GetMapping
     @Operation(description = "퀴즈디테일")
     public String detail(@RequestParam(required = false) Long idx){
         return quizService.findQuiz(idx).getDetail();
